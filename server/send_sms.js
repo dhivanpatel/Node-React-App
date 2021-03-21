7990ee7b8367022860e62984c3c112094f6878bc
dhivanp2 <dhivanp2@illinois.edu>
2021-03-20 22:08:41 -0500

    auth token fixed

--- server/send_sms.js
+++ server/send_sms.js
@@ -1,8 +1,8 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = "AC39e9a211d6030a4f27051c4f660eae22";
-const authToken = "4cb3aebd83f5948f75ae3c4a506aae68";
+const accountSid = process.env.ACCOUNTSID;
+const authToken = process.env.AUTHTOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 
